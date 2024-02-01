7bc103d3e1b362c297e9e5ccf3144a6ce8bec105
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-02-01 10:55:07 -0300

    fix: backend base url in local env

--- frontend/src/services/auth/authService.js
+++ frontend/src/services/auth/authService.js
@@ -1,6 +1,6 @@
 export const authService = {
   async login({ username, password }) {
-    return await fetch("http://localhost:4000/api/login", {
+    return await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
