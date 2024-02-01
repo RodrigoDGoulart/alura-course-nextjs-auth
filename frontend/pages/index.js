22b61fff0940deb2880507c8be90c963b412462a
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-02-01 10:19:42 -0300

    feat: onchange & submit event handle functions

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,22 +1,49 @@
+import React from "react";
+
 export default function HomeScreen() {
+  const [values, setValues] = React.useState({
+    usuario: "",
+    senha: "",
+  });
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setValues((current) => ({
+      ...current,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
-          placeholder="Usuário" name="usuario"
+          placeholder="Usuário"
+          name="usuario"
           defaultValue="omariosouto"
+          value={values.usuario}
+          onChange={handleChange}
         />
         <input
-          placeholder="Senha" name="senha" type="password"
+          placeholder="Senha"
+          name="senha"
+          type="password"
           defaultValue="safepassword"
+          value={values.senha}
+          onChange={handleChange}
         />
         <div>
-          <button>
-            Entrar
-          </button>
+          <button>Entrar</button>
         </div>
       </form>
+      {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
     </div>
   );
 }
